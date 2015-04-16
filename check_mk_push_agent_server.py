65acfcf11dcc0fce517a2fa44601629d2b16407a
Jens Gutermuth <jens@gutermuth-online.de>
2015-04-16 12:50:16 +0200

    added ReverseProxied
    Makes it easy to run in a subdirectory behind a reverse proxy such as
    nginx.

--- check_mk_push_agent_server.py
+++ check_mk_push_agent_server.py
@@ -7,10 +7,48 @@ from redis import StrictRedis
 
 from flask import Flask, abort, jsonify, request
 
-app = Flask('check_mk_push_agent_server')
-redis = StrictRedis()
-
 LIFETIME = 90
+
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    Source of this class: http://flask.pocoo.org/snippets/35/
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+app = Flask('check_mk_push_agent_server')
+proxy_app = ReverseProxied(app)
+redis = StrictRedis()
 
 
 def load_tokens():
